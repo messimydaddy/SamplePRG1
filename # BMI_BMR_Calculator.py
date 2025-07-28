cc36e4b7b855b383bed123e1d125be123671652b
messimydaddy <S10271064@connect.np.edu.sg>
2025-07-28 15:45:43 +0800

    This is my first commit

---
+++ # BMI_BMR_Calculator.py
@@ -0,0 +1,17 @@
+# BMI_BMR_Calculator.py
+# This program calculates the body mass index and basal metabolic rate
+
+# Calculate BMI
+weight = float(input('Enter weight(kg): '))
+height = input('Enter height(m): ')
+
+bmi = weight / height
+
+print('BMI: ', bmi)
+
+# Calculate BMR
+age = float(input('Enter age(years): '))
+
+bmr =  10 * weight + 6.25 * height - 5 * age + 5
+
+print('BMR: ', bmr)
